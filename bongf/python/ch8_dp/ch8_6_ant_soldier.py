216f58646e75cafe656cc885eae2bc9513a548f2
bong6981 <kbk5190@naver.com>
2021-09-06 16:21:04 +0900

    Complete ch8_6_ant_soldier(개미전사)

---
+++ bongf/python/ch8_dp/ch8_6_ant_soldier.py
@@ -0,0 +1,37 @@
+## 3,1,1,5 의 경우 기댓값 8 실제 결과값 6 
+def solution() :
+    n = int(input())
+    arr = list(map(int, input().split(" ")))
+
+    sum1 = 0 
+    sum2 = 0 
+
+    for i in range(n) :
+        if(i%2 == 0) :
+            sum1 += arr[i]
+        else :
+            sum2 += arr[i]
+    return max(sum1, sum2)
+
+## i번째 식량 창고에 있는 양이 k(i)라고 했을 때 점화식은 a(i) = max(a(i-1), a(i-2) + k(i))
+def dongbin() :
+    n = int(input())
+    array = list(map(int, input().split()))
+
+    d = [0] * 100 
+
+    d[0] = array[0]
+    d[1] = max(array[0], array[1])
+    for i in range(2, n):
+        print(i, n)
+        d[i] = max(d[i-1], d[i-2] + array[i])
+    return(d[n-1])
+
+# print(solution())
+print(dongbin())
+
+'''
+4
+1 3 1 5 
+결과 8
+'''
