1c76f0690f9315fa07d737cd98f89d0a49395adb
bong6981 <kbk5190@naver.com>
2021-09-06 16:20:19 +0900

    Complete ch8_5_make1(1로만들기)

---
+++ bongf/java/src/ch8_dp/Ch8_5_Make1.java
@@ -0,0 +1,39 @@
+package ch8_dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ch8_5_Make1 {
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+    public static int solution() {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if(x==1) {
+            return 1;
+        }
+        int[] d = new int[30001];
+        for(int i=2; i<x+1; i++) {
+            d[i] = d[i-1]+1;
+            if(i%2 == 0) {
+                d[i] = Math.min(d[i], d[i/2] + 1);
+            }
+            if(i%3==0) {
+                d[i] = Math.min(d[i], d[i/3] +1);
+            }
+            if(i%5==0) {
+                d[i] = Math.min(d[i], d[i/5] +1);
+            }
+        }
+        return d[x];
+    }
+
+}
+
+/*
+26
+결과값 3
+ */
