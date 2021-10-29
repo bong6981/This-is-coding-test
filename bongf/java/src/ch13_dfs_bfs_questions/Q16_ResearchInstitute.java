a936530318b4c84252806c5d94b394854e8b83e3
bong6981 <kbk5190@naver.com>
2021-10-29 21:10:15 +0900

    Complete Q16_ResearchInstitute.java(연구소)

---
+++ bongf/java/src/ch13_dfs_bfs_questions/Q16_ResearchInstitute.java
@@ -0,0 +1,85 @@
+package ch13_dfs_bfs_questions;
+
+import java.util.Scanner;
+
+public class Q16_ResearchInstitute {
+    public static int n;
+    public static int m;
+    public static int result = 0;
+    public static int[][] map;
+    public static int[][] temp;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        map = new int[n][m];
+        temp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+        dfs(0);
+        System.out.println(result);
+    }
+
+    public static void dfs(int cnt) {
+        if (cnt == 3) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    temp[i][j] = map[i][j];
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (temp[i][j] == 2) {
+                        virus(i, j);
+                    }
+                }
+            }
+
+            result = Math.max(result, getSafeZoneSize());
+            return;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j] = 1;
+                    dfs(cnt + 1);
+                    map[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private static void virus(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (temp[nx][ny] == 0) {
+                    temp[nx][ny] = 2;
+                    virus(nx, ny);
+                }
+            }
+        }
+    }
+
+    private static int getSafeZoneSize() {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (temp[i][j] == 0) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
