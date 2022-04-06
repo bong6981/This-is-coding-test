c596bcaf7eda4092f94a0e845606620fe6545368
bong6981 <kbk5190@naver.com>
2022-04-06 16:34:00 +0900

    doit/ch06_sort/bubblesort.py(버블소트기본)

--- bongf/java/src/doit/ch06_sort/BubbleSort.java
+++ bongf/java/src/doit/ch06_sort/BubbleSort.java
@@ -1,4 +1,28 @@
 package doit.ch06_sort;
 
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int[] x = new int[]{1, 3, 6, 4, 7, 8, 9};
+        bubbleSort(x, x.length);
+    }
+
+    private static void swap(int[] arr, int idx1, int idx2) {
+        int t = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = t;
+    }
+
+    private static void bubbleSort(int[] x, int length) {
+        for (int i = 0; i < x.length; i++) {
+            for (int j = x.length-1; j > i; j--) {
+                if(x[j-1] > x[j]) {
+                    swap(x, j-1, j);
+                }
+
+            }
+        }
+    }
 }
